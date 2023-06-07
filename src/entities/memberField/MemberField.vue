feba66536d480606cf91bce8ec0f85d1281bf6ca
Rim Khairullin <44344943+RimKhai@users.noreply.github.com>
2023-06-07 20:32:02 +0300

    Add/remove member added

--- src/entities/memberField/MemberField.vue
+++ src/entities/memberField/MemberField.vue
@@ -1,13 +1,18 @@
 <script setup>
+import { useMemberStore } from "../../stores/MembersStore"
+
+
+const memberStore = useMemberStore()
 const props = defineProps({
     name: String,
+    id: Number
 });
 </script>
 
 <template>
     <b-avatar class=""></b-avatar>
     <span class="">{{ name }}</span>
-    <b-button class="rounded-circle">-</b-button>
+    <b-button class="rounded-1" @click="memberStore.deleteMember(id)">-</b-button>
 </template>
 
 <style lang="scss" scoped>
--- src/widgets/membersList/MembersList.vue
+++ src/widgets/membersList/MembersList.vue
@@ -8,19 +8,18 @@ const memberStore = useMemberStore();
     <b-list-group>
         <b-list-group-item
             v-for="homie in memberStore.homies"
-            class="members-item rounded-4 p-2 d-flex justify-content-between align-items-center"
+            class="members-item rounded-2 p-2 d-flex justify-content-between align-items-center"
             :key="homie.cost"
         >
-            <MemberField :name="homie.name"></MemberField>
+            <MemberField :name="homie.name" :id="homie.id"></MemberField>
         </b-list-group-item>
     </b-list-group>
 </template>
 
 <style lang="scss" scoped>
 .members-item {
-    width: $ui-step*100;
+    width: $ui-step * 100;
     background-color: $additional-color;
-    margin-top: $ui-step;
-    
+    margin-top: $ui-step * 2;
 }
 </style>
